1fbf492c59378360f0d59cdc8e57b7b5b0ce9605
yadnesh <yadneshtendolkar.06@gmail.com>
2024-05-02 21:09:48 +0530

    skill section position fixed

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -6,7 +6,7 @@ import skillsData from '../constants/skills.js';
 function skills() {
   return (
     <>
-      <div id='skills' className='skillsElemet w-full h-screen box-border md:pr-24 flex flex-wrap rounded-t-3xl bg-orange-400 pt-[20vh] md:pt-[25vh] '>
+      <div id='skills' className='skillsElemet w-full h-screen box-border md:pr-24 flex flex-col md:flex-row rounded-t-3xl bg-orange-400 pt-[20vh] md:pt-[25vh] '>
         <div className='text-7xl md:text-8xl font-poppins font-bold md:mt-[20vh] ml-[25vw] md:ml-56' id='skills'>
           <h1>Skills</h1>
         </div>
