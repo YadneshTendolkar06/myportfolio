b4c840fcf30fdb6ac42ea1635235abe3efd6f1f7
yadnesh <yadneshtendolkar.06@gmail.com>
2024-10-07 19:14:14 +0530

    resume updated

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -21,7 +21,7 @@ function introduction({reference}) {
   }}
 />
               <div className='mb-2 md:mt-12 w-[25vw] md:w-[10vw] md:h-[6vh] flex justify-center items-center rounded-full text-[3vh] text-zinc-800 bg-zinc-100 font-poppins '>
-                <a href='https://drive.google.com/file/d/1Fovvt8B4xyWAk3MNhsGvhrT3aAfa0eTh/view?usp=sharing'><button>Resume</button></a>
+                <a href='https://drive.google.com/file/d/1FhkjmX8RaKcVyuiDDMAy6_xDUdYHeJ-g/view?usp=sharing'><button>Resume</button></a>
               </div>
 
             </div>
